6aff0ac32c5f6f3e10f5f502413eeec385cbf632
Khagesh Sharma <khageshsharma24@gmail.com>
2024-07-25 22:05:01 +0530

    Responsiveness of the home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -19,26 +19,26 @@ export default function Home() {
   return (
     <div>
       <Nav />
-      <div className="lg:grid-cols-2 md:grid-cols-2 grid grid-cols-1">
-        <div className="flex flex-col lg:pl-20 md:pl-20 justify-center">
+      <div className="lg:grid-cols-2 grid grid-cols-1">
+        <div className="flex flex-col lg:pl-20 justify-center">
           <div>
-            <div className="lg:text-6xl md:text-4xl sm:text-3xl text-2xl lg:text-left md:text-left text-center flex flex-col gap-4 font-spacegrotesksemibold">
+            <div className="lg:text-6xl md:text-4xl sm:text-3xl text-2xl lg:text-left text-center flex flex-col gap-4 font-spacegrotesksemibold">
               <div>
                 Speak.<span className="text-[#2D8CFF]"> Learn</span>. Thrive
               </div>
               <div>Bridging the gap with</div>
               <div>every word</div>
             </div>
-            <div className="lg:hidden md:hidden flex justify-center mt-10">
+            <div className="lg:hidden flex justify-center mt-10">
               <img src={Mic} />
             </div>
-            <div className="lg:text-xl md:text-xl text-lg font-spacegroteskregular my-10 text-center lg:text-start md:text-start p-1 lg:p-0 md:p-0">
+            <div className="lg:text-xl md:text-xl text-lg font-spacegroteskregular my-10 text-center lg:text-start p-1 lg:p-0">
               Our goal is to empower individuals with speech challenges. Unlock
               your potential through personalized speech training.
             </div>
           </div>
 
-          <div className="flex flex-col lg:flex-row md:flex-row sm:flex-row gap-8 sm:justify-center md:justify-start">
+          <div className="flex flex-col lg:flex-row sm:flex-row lg:justify-start gap-8 sm:justify-center">
             <div className="flex justify-center">
               <button className="border rounded-md font-spacegroteskmedium flex items-center justify-center px-12 py-4 bg-[#89D85D] text-black hover:bg-opacity-70 border-black">
                 <p>Get Started</p>
@@ -52,21 +52,21 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="hidden lg:flex md:flex justify-center">
+        <div className="hidden lg:flex justify-center">
           <img src={Mic} />
         </div>
       </div>
 
       <div>
-        <div className="flex items-center justify-center lg:block md:block">
-          <div className="border-y-4 lg:mt-36 md:mt-32 sm:mt-14 mt-16 font-spacegrotesksemibold border-black font-medium lg:text-4xl md:text-3xl sm:text-2xl text-2xl w-fit lg:ml-20 md:ml-20 text-center">
+        <div className="flex items-center justify-center lg:block">
+          <div className="border-y-4 lg:mt-36 md:mt-32 sm:mt-14 mt-16 font-spacegrotesksemibold border-black font-medium lg:text-4xl md:text-3xl sm:text-2xl text-2xl w-fit lg:ml-20 text-center">
             Features
           </div>
         </div>
 
         <div className="">
           <div className="flex flex-wrap items-center justify-center gap-8 p-10">
-            <div className="bg-[#2D8CFF] rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96">
+            <div className="bg-[#2D8CFF] rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96 sm:w-96 max-w-96">
               <div className="flex items-center justify-center lg:items-start lg:justify-start md:items-start md:justify-start">
                 <img
                   src={Engaging}
@@ -82,7 +82,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="bg-white rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96">
+            <div className="bg-white rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96 max-w-96">
               <div className="flex items-center justify-center lg:items-start lg:justify-start md:items-start md:justify-start">
                 <img
                   src={Progress}
@@ -98,7 +98,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="bg-[#2D8CFF] rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96">
+            <div className="bg-[#2D8CFF] rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96 max-w-96">
               <div className="flex items-center justify-center lg:items-start lg:justify-start md:items-start md:justify-start">
                 <img src={Phonic} alt="" className="lg:h-24 md:h-24 h-14" />
               </div>
@@ -111,7 +111,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="bg-white rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96">
+            <div className="bg-white rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96 max-w-96">
               <div className="flex items-center justify-center lg:items-start lg:justify-start md:items-start md:justify-start">
                 <img src={Motor} alt="" className="mb-4 lg:h-20 md:h-20 h-14" />
               </div>
@@ -123,7 +123,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="bg-[#2D8CFF] rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96">
+            <div className="bg-[#2D8CFF] rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96 max-w-96">
               <div className="flex items-center justify-center lg:items-start lg:justify-start md:items-start md:justify-start">
                 <img src={Visual} alt="" className="lg:h-24 md:h-24 h-14" />
               </div>
@@ -136,7 +136,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="bg-white rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96">
+            <div className="bg-white rounded-3xl p-6 shadow-2xl lg:h-64 lg:w-96 md:h-64 md:w-96 max-w-96">
               <div className="flex items-center justify-center lg:items-start lg:justify-start md:items-start md:justify-start">
                 <img
                   src={Learning}
@@ -157,28 +157,32 @@ export default function Home() {
       </div>
 
       <div>
-        <div className="flex items-center justify-center lg:block md:block">
-          <div className="border-y-4 p-1 lg:mt-36 md:mt-32 sm:mt-14 mt-16 font-spacegrotesksemibold border-black font-medium lg:text-4xl md:text-3xl sm:text-2xl text-2xl w-fit lg:ml-20 md:ml-20 text-center">
+        <div className="flex items-center justify-center lg:block">
+          <div className="border-y-4 p-1 lg:mt-36 md:mt-32 sm:mt-14 mt-16 font-spacegrotesksemibold border-black font-medium lg:text-4xl md:text-3xl sm:text-2xl text-2xl w-fit lg:ml-20 text-center">
             Strengths
           </div>
         </div>
 
         <div>
-          <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1">
-            <div className="lg:ml-20 md:ml-20 flex flex-col gap-8 items-center justify-center">
-              <div className="lg:hidden md:hidden flex justify-center items-center mt-10">
-                <img src={RealTIme} alt="" className="h-[200px]" />
+          <div className="grid lg:grid-cols-2 grid-cols-1">
+            <div className="lg:ml-20 flex flex-col gap-8 items-center justify-center">
+              <div className="lg:hidden flex justify-center items-center mt-10">
+                <img
+                  src={RealTIme}
+                  alt=""
+                  className="h-[200px] md:h-[300px] sm:h-[300px]"
+                />
               </div>
-              <div className="lg:text-4xl md:text-3xl text-xl p-1 lg:p-0 md:p-0 lg:text-start md:text-start text-center font-spacegrotesksemibold">
+              <div className="lg:text-4xl md:text-3xl text-xl p-1 lg:p-0 lg:text-start text-center font-spacegrotesksemibold">
                 Real-Time Speech Detection and Weekly Test Analysis
               </div>
-              <div className="font-spacegroteskregular lg:text-xl md:text-xl text-sm lg:text-start md:text-start text-center p-4 lg:p-0 md:p-0">
+              <div className="font-spacegroteskregular lg:text-xl md:text-xl text-sm lg:text-start text-center p-4 lg:p-0">
                 Unlock the potential of your voice through cutting-edge
                 technology. Our platform not only hears your words but guides
                 you towards eloquence with precision.
               </div>
             </div>
-            <div className="hidden lg:flex md:flex justify-center">
+            <div className="hidden lg:flex justify-center">
               <img src={RealTIme} alt="" className="h-[400px]" />
             </div>
           </div>
@@ -191,11 +195,11 @@ export default function Home() {
                 className="lg:h-[400px] md:h-[400px] h-[200px]"
               />
             </div>
-            <div className="lg:ml-20 md:ml-20 ml-0 flex flex-col gap-8 items-center justify-center lg:mt-0 md:mt-0 mt-10">
-              <div className="lg:text-4xl md:text-3xl text-xl p-1 lg:p-0 md:p-0 lg:text-start md:text-start text-center font-spacegrotesksemibold">
+            <div className="lg:ml-20 ml-0 flex flex-col gap-8 items-center justify-center lg:mt-0 md:mt-0 mt-10">
+              <div className="lg:text-4xl md:text-3xl text-xl p-1 lg:p-0 lg:text-start text-center font-spacegrotesksemibold">
                 Real-Time Speech Detection and Weekly Test Analysis
               </div>
-              <div className="font-spacegroteskregular lg:text-xl md:text-xl text-sm lg:text-start md:text-start text-center p-4 lg:p-0 md:p-0">
+              <div className="font-spacegroteskregular lg:text-xl md:text-xl text-sm lg:text-start text-center p-4 lg:p-0">
                 Unlock the potential of your voice through cutting-edge
                 technology. Our platform not only hears your words but guides
                 you towards eloquence with precision.
@@ -208,16 +212,16 @@ export default function Home() {
       <div>
         <div className="bg-[#F3F3F3] lg:my-32 md:my-32 my-10 mx-4 md:mx-32 rounded-3xl">
           <div className="grid lg:grid-cols-2">
-            <div className="text-center p-10 flex flex-col items-start justify-center gap-6">
+            <div className="text-center p-10 flex flex-col lg:items-start items-center justify-center gap-6">
               <div className="lg:text-4xl md:text-3xl text-2xl font-spacegroteskmedium">
                 Let's Make things happen!
               </div>
-              <div className="font-spacegroteskregular lg:text-lg md:text-lg text-sm lg:text-start md:text-start text-center">
+              <div className="font-spacegroteskregular lg:text-lg md:text-lg text-sm lg:text-start text-center">
                 To get started and gain some insights and knowledge about speech
                 disorders, how to cure them and related stuff go to the articles
                 section.
               </div>
-              <div className="flex justify-center items-center w-full lg:justify-start md:justify-start">
+              <div className="flex justify-center items-center w-full lg:justify-start">
                 <button className="p-4 bg-[#89D85D] rounded-md font-spacegroteskmedium hover:bg-opacity-90">
                   Read Articles →
                 </button>
@@ -235,8 +239,8 @@ export default function Home() {
       </div>
 
       <div>
-        <div className="flex items-center justify-center lg:block md:block">
-          <div className="border-y-4 p-1 lg:mt-36 md:mt-32 sm:mt-14 mt-16 font-spacegrotesksemibold border-black font-medium lg:text-4xl md:text-3xl sm:text-2xl text-2xl w-fit lg:ml-20 md:ml-20 text-center">
+        <div className="flex items-center justify-center lg:block">
+          <div className="border-y-4 p-1 lg:mt-36 md:mt-32 sm:mt-14 mt-16 font-spacegrotesksemibold border-black font-medium lg:text-4xl md:text-3xl sm:text-2xl text-2xl w-fit lg:ml-20 text-center">
             Contact Us
           </div>
         </div>
@@ -250,7 +254,7 @@ export default function Home() {
             <div>
               <div className="lg:p-8 md:p-8 p-2">
                 <div className="flex items-center justify-center">
-                  <div className="font-spacegroteskmedium lg:text-4xl md:text-3xl hidden lg:block md:block">
+                  <div className="font-spacegroteskmedium lg:text-4xl md:text-3xl hidden lg:block">
                     Connect With Us
                   </div>
                 </div>
